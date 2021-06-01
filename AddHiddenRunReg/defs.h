f022c53c18f7e1551afe5f6e8c9e4f8d6d38e615
Nek0y4nSu <nekoyansu@outlook.com>
2021-06-01 12:48:15 +0800

    Add files via upload

---
+++ AddHiddenRunReg/defs.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <Windows.h>
+
+#define STATUS_BUFFER_OVERFLOW	((NTSTATUS)0x80000005L)
+#define NT_SUCCESS(x) ((x)>=0)
+#define STATUS_SUCCESS ((NTSTATUS)0)
+#define OBJ_CASE_INSENSITIVE 64L
+
+typedef struct _LSA_UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} LSA_UNICODE_STRING, *PLSA_UNICODE_STRING, UNICODE_STRING, *PUNICODE_STRING;
+
+
+typedef struct _OBJECT_ATTRIBUTES {
+	ULONG Length;
+	HANDLE RootDirectory;
+	PUNICODE_STRING ObjectName;
+	ULONG Attributes;
+	PVOID SecurityDescriptor;
+	PVOID SecurityQualityOfService;
+} OBJECT_ATTRIBUTES,*POBJECT_ATTRIBUTES;
+
+typedef NTSTATUS(__stdcall *PROC_NtOpenKey)(
+	PHANDLE KeyHandle,
+	ACCESS_MASK DesiredAccess,
+	POBJECT_ATTRIBUTES ObjectAttributes
+	);
+
+typedef VOID(__stdcall *PROC_RtlInitUnicodeString)(
+	PUNICODE_STRING DestinationString,
+	PCWSTR SourceString
+	);
+typedef NTSTATUS(__stdcall *PROC_NtCreateKey)(
+	PHANDLE KeyHandle,
+	ACCESS_MASK DesiredAccess,
+	POBJECT_ATTRIBUTES ObjectAttributes,
+	ULONG TitleIndex,
+	PUNICODE_STRING Class,
+	ULONG CreateOptions,
+	PULONG Disposition
+	);
+typedef NTSTATUS(__stdcall * PROC_NtDeleteKey)(
+	HANDLE KeyHandle
+);
+typedef NTSTATUS(__stdcall *PROC_NtSetValueKey)(
+	HANDLE KeyHandle,
+	PUNICODE_STRING ValueName,
+	ULONG TitleIndex,
+	ULONG Type,
+	PVOID Data,
+	ULONG DataSize
+	);
+typedef NTSTATUS(__stdcall *PROC_NtClose)(
+	HANDLE Handle
+	);
+
+typedef NTSTATUS(__stdcall *PROC_NtDeleteValueKey)
+(
+	IN HANDLE			KeyHandle,
+	IN PUNICODE_STRING	ValueName
+	);
+
+PROC_NtDeleteKey _NtDeleteKey;
+PROC_NtCreateKey _NtCreateKey;
+PROC_NtOpenKey _NtOpenKey;
+PROC_NtSetValueKey _NtSetValueKey;
+PROC_RtlInitUnicodeString _RtlInitUnicodeString;
+PROC_NtClose _NtClose;
+PROC_NtDeleteValueKey _NtDeleteValueKey;
---
+++ AddHiddenRunReg/readme.txt
@@ -0,0 +1,1 @@
+use api add hidden value key 
